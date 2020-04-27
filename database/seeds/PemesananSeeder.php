965f60a57b0e64cab07832e0109a3b6b93a5137c
Rafi <pilotrogue@gmail.com>
2020-04-27 20:37:32 +0700

    revert seeder

--- database/seeds/PemesananSeeder.php
+++ database/seeds/PemesananSeeder.php
@@ -20,7 +20,7 @@ class PemesananSeeder extends Seeder
                 'id_guru' => 6,
                 'id_murid' => 2,
                 'id_mapel' => 1,
-                'created_at' => '2020-03-16 12:04:21',
+                'waktu_pemesanan' => '2020-03-16 12:04:21',
                 'kelas' => 5,
                 'status' => 1,
                 'jumlah_pertemuan' => 12
@@ -29,7 +29,7 @@ class PemesananSeeder extends Seeder
                 'id_guru' => 6,
                 'id_murid' => 2,
                 'id_mapel' => 2,
-                'created_at' => '2020-04-20 10:19:10',
+                'waktu_pemesanan' => '2020-04-20 10:19:10',
                 'kelas' => 5,
                 'status' => 0,
                 'jumlah_pertemuan' => 0
@@ -38,7 +38,7 @@ class PemesananSeeder extends Seeder
                 'id_guru' => 6,
                 'id_murid' => 2,
                 'id_mapel' => 2,
-                'created_at' => '2020-04-04 19:45:24',
+                'waktu_pemesanan' => '2020-04-04 19:45:24',
                 'kelas' => 5,
                 'status' => 3,
                 'jumlah_pertemuan' => 2
@@ -47,7 +47,7 @@ class PemesananSeeder extends Seeder
                 'id_guru' => 6,
                 'id_murid' => 4,
                 'id_mapel' => 3,
-                'created_at' => '2020-04-01 13:42:14',
+                'waktu_pemesanan' => '2020-04-01 13:42:14',
                 'kelas' => 1,
                 'status' => 2,
                 'jumlah_pertemuan' => 0
