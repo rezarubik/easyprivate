32d187e6431be58b2095bb46a2888b8dba995608
Rafi <pilotrogue@gmail.com>
2020-06-22 16:04:24 +0700

    benerin absen

--- app/JadwalPengganti.php
+++ app/JadwalPengganti.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class JadwalPengganti extends Model
 {
     protected $table = 'jadwal_penggantis';
-    protected $primaryKey = 'id';
+    protected $primaryKey = 'id_jadwal_pengganti';
     protected $fillable = ['id_pemesanan', 'id_jadwal_pemesanan', 'waktu_pengganti'];
 
     public function absen()
     {
-        return $this->belongsTo('App\Absen', 'id_jadwal_pengganti', 'id');
+        return $this->belongsTo('App\Absen', 'id_jadwal_pengganti', 'id_jadwal_pengganti');
     }
 }
